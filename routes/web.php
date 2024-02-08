3795e6610d6963be4cb977f21e6a4179938c20cd
Ismael Gomes <ismael.gomes.l70@gmail.com>
2024-02-08 11:08:07 -0400

    update

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AtendenteController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\ContatoController;
 use App\Http\Controllers\ProfileController;
@@ -16,8 +17,12 @@ Route::get('/login', [AuthenticatedSessionController::class, 'create'])->name('l
 route::get('/', [ContatoController::class, 'show_contato']);
 route::post('/dashboard', [ContatoController::class, 'store']);
 route::get('/dashboard/edit/{id}', [ContatoController::class, 'edit']);
+//TESTE API DO BC
+route::get('/form', [ContatoController::class, 'chamada']);
 
 Route::middleware('auth')->group(function () {
+    route::post('/atendente', [AtendenteController::class, 'store']);
+
     route::put('/dashboard/{id}', [ContatoController::class, 'update']);
     route::delete('/dashboard/{id}', [ContatoController::class, 'destroy']);
     route::get('/dashboard', [ContatoController::class, 'index'])->name('dashboard');
